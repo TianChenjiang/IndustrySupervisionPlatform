b60eab5389f5bf0e902a420ebd6eed3c0384c31c
TianChenjiang <tiancj0310@outlook.com>
2019-04-06 23:39:06 +0800

    backend initial commit

---
+++ backend/src/main/java/dachuang/industry/supervision/backend/platform/PlatformApplication.java
@@ -0,0 +1,13 @@
+package dachuang.industry.supervision.backend.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+
+}
